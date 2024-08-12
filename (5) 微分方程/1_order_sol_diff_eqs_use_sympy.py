eea12f47eb478e1ebd4272dbf88b286572d7ce24
aFei-CQUT <2039787966@qq.com>
2024-08-12 18:41:49 +0800

    求解微分方程组

---
+++ (5) 微分方程/1_order_sol_diff_eqs_use_sympy.py
@@ -0,0 +1,34 @@
+import sympy as sp
+
+# 定义符号变量
+x = sp.symbols('x')
+y1 = sp.Function('y1')(x)
+y2 = sp.Function('y2')(x)
+
+# 定义一阶微分方程
+ode1 = sp.Eq(y1.diff(x) + y1, 0)
+ode2 = sp.Eq(y2.diff(x) - 2*y2, 0)
+
+# 分别求解每个方程
+solution_y1 = sp.dsolve(ode1)
+solution_y2 = sp.dsolve(ode2)
+
+# 方程组的解
+combined_solution = {
+    'y1': solution_y1,
+    'y2': solution_y2
+}
+
+print("\n该一阶微分方程组的解:")
+for key, sol in combined_solution.items():
+    print(f"{key}: {sol}")
+
+# 打印LaTeX格式的输出
+latex_output_y1 = sp.latex(solution_y1)
+latex_output_y2 = sp.latex(solution_y2)
+
+print("\n第一个方程解的LaTeX格式输出:")
+print(latex_output_y1)
+
+print("\n第二个方程解的LaTeX格式输出:")
+print(latex_output_y2)
